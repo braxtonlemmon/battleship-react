750f97aa6926394cae05728d2a69eb0de2378988
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-02-25 14:55:37 +0100

    Test and implement placeShip and receiveAttack methods

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,27 +1,52 @@
+import Ship from './Ship.js';
 
 // based on a row,col coordinate system .... board[row][col] ...board[row][column]
 const Gameboard = () => {
-  const board = new Array(10); //.fill(Array(10).fill(null));
-  for(let i = 0; i < 10; i ++) {
-    board[i] = new Array(10).fill(null);
-  }
+  const board = new Array(10).fill(new Array(10).fill(null));
+  // for(let i = 0; i < 10; i ++) {
+  //   board[i] = new Array(10).fill(null);
+  // }
+  const ships = [];
 
-  const placeShip = (length, row, col, direction) => {
-    const coords = desiredPlacement(length, row, col, direction);
-    if (isOnBoard(coords) && isAvailable(coords)) {
-      coords.forEach(coord => {
-        const row = coord[0];
-        const col = coord[1];
-        board[row][col] = 'x';
-      })  
-    } 
-  }
+  const placeShip = (ship, row, col) => {
+      // a board cell should know:
+      // (1) its coordinates <row, col>
+      // if ship is occupying space:
+      // (2) ship id
+      // (3) ship segment's position number
+
+      const { length, direction } = ship;
+
+      const coords = desiredPlacement(length, row, col, direction);
+      if (isOnBoard(coords) && isAvailable(coords)) {
+        coords.forEach((coord, position) => {
+          const row = coord[0];
+          const col = coord[1];
+          board[row][col] = `${ship.id}${position}`;
+        })
+        ships.push({ ship, coords })  
+      } 
+    }
 
   const receiveAttack = (row, col) => {
-    return true;
+    if (board[row][col] === null) {
+      board[row][col] = 'M';
+    } 
+    else if (board[row][col] !== 'M' || board[row][col] !== 'X') {
+      const id = parseInt(board[row][col].split('')[0]);
+      const index = parseInt(board[row][col].split('')[1]);
+      const ship = getShip(id);
+      ship.hit(index);
+      board[row][col] = 'X';
+    }
   }  
   // PRIVATE 
 
+  const getShip = (id) => {
+    const foundShip = ships.find(item => item.ship.id === id);
+    return foundShip.ship;
+  }
+  
   const desiredPlacement = (length, row, col, direction) => {
     let coordinates = [];
     for(let i = 0; i < length; i ++) {
@@ -53,6 +78,5 @@ const Gameboard = () => {
 }
 
 export default Gameboard;
-
 
 
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -11,23 +11,33 @@ it('has 10 columns in a row', () => {
 });
 
 it('places a ship if coordinates are valid', () => {
-  const board = Gameboard();;
-  board.placeShip(3, 1, 1, 'horizontal');
+  const board = Gameboard();
+  const ship = { length: 3, direction: 'horizontal' };
+  board.placeShip(ship, 1, 1);
   expect(board.board[1][1]).toBeTruthy();
   expect(board.board[1][2]).toBeTruthy();
   expect(board.board[1][3]).toBeTruthy();
 });
 
+it('marks placed ship with ship id', () => {
+  const board = Gameboard();
+  const ship = { length: 3, direction: 'horizontal', id: 1 };
+  board.placeShip(ship, 1, 1);
+  expect(board.board[1][1]).toBe(`${ship.id}0`);
+})
+
 it('does not place ship if goes off board', () => {
   const board = Gameboard();
-  board.placeShip(3, 5, 8, 'horizontal');
+  const ship = { length: 3, direction: 'horizontal', id: 1 };
+  board.placeShip(ship, 1, 1);
   expect(board.board[5][8]).toBeFalsy();
 });
 
 it('does not place ship if location not empty', () => {
   const board = Gameboard();
   board.board[1][1] = 'x';
-  board.placeShip(3, 1, 1, 'horizontal');
+  const ship = { length: 3, direction: 'horizontal', id: 1 };
+  board.placeShip(ship, 1, 1);
   expect(board.board[1][2]).toBeFalsy();
   expect(board.board[1][1]).toBeTruthy();
 });
@@ -36,3 +46,18 @@ it('receives an attack', () => {
   const board = Gameboard();
   board.receiveAttack(3, 1);
 })
+
+it('marks "M" if spot is empty', () => {
+  const board = Gameboard();
+  board.receiveAttack(3, 1);
+  expect(board.board[3][1]).toBe('M');
+});
+
+it('marks "X" if spot is occupied and not hit', () => {
+  const board = Gameboard();
+  const ship = { length: 3, direction: 'horizontal', id: 1 };
+  board.placeShip(ship, 1, 1);
+  board.receiveAttack(1, 1);
+  expect(board.board[1][1]).toBe('X');
+})
+
