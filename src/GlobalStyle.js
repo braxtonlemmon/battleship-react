d4d3f1885729fd0871a9e1b51b197ffdea79921f
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-23 10:36:14 +0100

    Change background image

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body, html {
-    background: grey;
+    background: url('https://images.pexels.com/photos/734973/pexels-photo-734973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
     box-sizing: border-box;
   }
 
