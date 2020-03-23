e31dff3d20a8e280afd99c3d0b1998daa07d502e
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-23 11:27:54 +0100

    Update Rules styling.

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const RULES = [
-  'Welcome to Battleship! The game is played with two players. To setup the game, each player places five ships of varying lengths on a personal gameboard. Ships can be positioned horizontally or vertically on the board. They may touch, but may not occupy the same area. All ships must remain within the gameboard boundaries.',
+  'Welcome to Battleship! You will be playing against a computer. To setup the game, each player places five ships of varying lengths on a personal gameboard by clicking or dragging. Ships can be positioned horizontally or vertically on the board. They may touch, but may not occupy the same area. All ships must remain within the gameboard boundaries.',
 
-  'Players alternate taking turns. On a turn, a player attacks the opponent\'s gameboard by clicking on an unoccupied square. If the attacks hits an enemy ship, the square will turn red. Otherwise, the attack is considered a miss and the square will turn orange.',
+  'Players alternate taking turns. On a turn, a player attacks the opponent\'s gameboard by clicking on an unoccupied square. If the attacks hits an enemy ship, the square will turn red. Otherwise, the attack is considered a miss and the square will turn grey.',
 
   'When a player sinks an enemy ship, the player will be notified. The game ends once one player has destroyed all enemy ships.'
 ];
